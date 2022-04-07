a69e072167a30174a38eebd34a0a6b32e2134a81
Pritam Sen <pritam.sen.2406@gmail.com>
2022-04-07 01:45:31 -0400

    Deleting resources

--- ProbashBondhu/ProbashBondhu/PBConstants.swift
+++ ProbashBondhu/ProbashBondhu/PBConstants.swift
@@ -11,8 +11,7 @@ import UIKit
 
 struct PBConstants
 {
-    static let urlPrefix = "https://probashbondhu.synesisit.com.bd:9600"//"http://probashbondhu.synesisit.com.bd:9500"
-    
+    static let urlPrefix = "https://probashbondhu.**********:9600"    
     static let commonBlueColor = UIColor(displayP3Red: 70.0/255.0, green: 108.0/255.0, blue: 210.0/255.0, alpha: 1.0)
     
     static let commonGreyColor = UIColor(displayP3Red: 0.0/255.0, green: 108.0/255.0, blue: 210.0/255.0, alpha: 1.0)
